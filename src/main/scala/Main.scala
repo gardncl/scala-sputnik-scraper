e3638b5cdbc24f6a1bcf03d540dae184232b05ca
gardncl <gardncl@gmail.com>
2018-01-20 22:55:55 -0500

    Conflict on akka actor version, why?

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,36 @@
+import akka.actor.{ActorSystem, Props}
+import io.bfil.scalescrape.actor.ScrapingActor
+
 object Main extends App {
 
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[ExampleScraper], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
 }
+
+
+class ExampleScraper extends ScrapingActor {
+
+  private val baseUrl = "https://www.sputnikmusic.com/"
+
+  override def receive: Receive = {
+    case _       => grabMainTitle(baseUrl)
+  }
+
+  private def grabMainTitle(url: String) =
+    scrape {
+          get(url) { response =>
+            complete(doIt(response))
+          }
+    }
+
+  private def doIt(response: Any): Unit = {
+    println(response)
+  }
+}
+
+
+
+
