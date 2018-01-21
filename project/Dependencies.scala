8d1b94607c08e35a6a0d73e0aecae9f43fda945c
gardncl <gardncl@gmail.com>
2018-01-21 07:52:22 -0500

    resolved one error, but got another

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,25 @@ import sbt._
 object Dependencies {
 
   object v {
+    val cats = "0.9.0"
+    val logback = "1.2.3"
+    val scalacheck = "1.13.4"
+    val scalalogging = "3.7.2"
+    val scalatest = "3.0.1"
     val akka = "2.5.8"
     val scrape = "0.4.0"
+    val slick = "3.2.1"
+    val slickpg = "0.15.3"
   }
 
   val akka = Seq(
-    "com.typesafe.akka" %% "akka-actor" % v.akka
+    "com.typesafe.akka" %% "akka-actor" % v.akka,
+    "com.typesafe.akka" %% "akka-slf4j" % v.akka,
+    "com.typesafe.akka" %% "akka-stream" % v.akka
   )
 
   val scrape = Seq(
     "io.bfil" %% "scalescrape" % v.scrape
   )
-
-  lazy val allDeps =  akka ++ scrape
+  lazy val allDeps = akka ++ scrape
 }
