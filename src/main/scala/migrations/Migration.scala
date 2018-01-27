bd8faffd45b9249f6fce341a26e93cba780fddc9
gardncl <gardncl@gmail.com>
2018-01-26 22:03:05 -0500

    Get migrations code set up

--- src/main/scala/migrations/Migration.scala
+++ src/main/scala/migrations/Migration.scala
@@ -1,7 +1,7 @@
 package migrations
 
-
 import slick.dbio.DBIO
+import slick.jdbc.PostgresProfile.api._
 import slick.migration.api.{SqlMigration, Migration => ApiMigration}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -10,37 +10,37 @@ trait Migration {
   val name: String
   val migration: ApiMigration
 
-//  def run(implicit ec: ExecutionContext, db: Database): Future[Boolean] = {
-//    isPending.flatMap { pending =>
-//      if (pending) {
-//        db.run(
-//          DBIO
-//            .seq(
-//              migration(),
-//              addMigration()
-//            )
-//            .transactionally
-//        )
-//          .map(_ => true)
-//      } else {
-//        Future(pending)
-//      }
-//    }
-//  }
+  def run(implicit ec: ExecutionContext, db: Database): Future[Boolean] = {
+    isPending.flatMap { pending =>
+      if (pending) {
+        db.run(
+          DBIO
+            .seq(
+              migration(),
+              addMigration()
+            )
+            .transactionally
+        )
+          .map(_ => true)
+      } else {
+        Future(pending)
+      }
+    }
+  }
 
   def addMigration: ApiMigration = {
     SqlMigration(s"INSERT INTO migrations (name) VALUES ('${name}')")
   }
 
-//  def isPending(implicit ec: ExecutionContext,
-//                db: Database): Future[Boolean] = {
-//    val presentQuery: DBIO[Seq[(Int)]] = sql"""
-//          SELECT
-//            1
-//          FROM
-//            migrations
-//          WHERE
-//            migrations.name = ${name}""".as[Int]
-//    db.run(presentQuery).map(_.isEmpty)
-//  }
+  def isPending(implicit ec: ExecutionContext,
+                db: Database): Future[Boolean] = {
+    val presentQuery: DBIO[Seq[(Int)]] = sql"""
+          SELECT
+            1
+          FROM
+            migrations
+          WHERE
+            migrations.name = ${name}""".as[Int]
+    db.run(presentQuery).map(_.isEmpty)
+  }
 }
